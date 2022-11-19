6a12d75864dc8925b054e1be202dc9630331237a
Poul <poul.pheez2@gmail.com>
2022-11-19 19:35:22 -0300

    integrando js

---
+++ calculadora.js
@@ -0,0 +1,9 @@
+scri
+
+function inserir(numero) {
+
+    var numeral = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = numeral + numero;
+
+
+}
