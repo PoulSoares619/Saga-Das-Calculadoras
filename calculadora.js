e6af34bb844ceb2b3a662652a6393fcfda5d8ac8
Poul <poul.pheez2@gmail.com>
2022-11-19 20:09:19 -0300

    quase pronto

--- calculadora.js
+++ calculadora.js
@@ -1,4 +1,4 @@
-scri
+
 
 function inserir(numero) {
 
@@ -6,4 +6,31 @@ function inserir(numero) {
     document.getElementById('resultado').innerHTML = numeral + numero;
 
 
+}
+
+function limpaResultado() {
+
+    document.getElementById('resultado').innerHTML = "";
+}
+
+function limpaUm() {
+
+    var resultado = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
+
+}
+
+function calculando() {
+
+    var resultado = document.getElementById('resultado').innerHTML;
+    
+    if(resultado) {
+
+        document.getElementById('resultado').innerHTML = eval(resultado);
+
+    }  else {
+
+        document.getElementById('resultado').innerHTML = "";
+
+    }
 }
